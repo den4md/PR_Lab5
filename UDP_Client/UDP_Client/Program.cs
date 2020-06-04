98370fdcf6b1f0cc4fbbd3f840354f6a4919012c
den4md <denisstarr@gmail.com>
2020-06-04 18:52:38 +0300

    Client-Server screensharing

---
+++ UDP_Client/UDP_Client/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            byte[] byteScreenshot;
+            int count_array = 0;
+
+            System.Console.WriteLine("Нажмите <Enter> для старта трансляции");
+            System.Console.ReadLine();
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ExclusiveAddressUse, 1);
+            IPEndPoint ipEndPoint = new IPEndPoint(ip, 16000);
+
+            byte[] idle = new byte[0];
+            socket.SendTo(idle, ipEndPoint);
+
+            System.Console.WriteLine("Трансляция началась!");
+
+            do
+            {
+                Thread.Sleep(5);
+                int position = 0;
+
+                byteScreenshot = MakeScreenshot();
+
+                List<byte[]> fragments = new List<byte[]>();
+
+                if (byteScreenshot.Length % 65002 != 0)
+                {
+                    count_array = byteScreenshot.Length / 65002 + 1;
+                }
+                else
+                    count_array = byteScreenshot.Length / 65002;
+
+                while (position < byteScreenshot.Length)
+                {
+                    byte[] fragmentScreenshot;
+                    if ((byteScreenshot.Length - position) >= 65000)
+                        fragmentScreenshot = new byte[65002];
+                    else
+                    {
+                        fragmentScreenshot = new byte[byteScreenshot.Length - position + 2];
+                    }
+
+                    Array.Copy(byteScreenshot, position, fragmentScreenshot, 0,
+                        fragmentScreenshot.Length - 2);
+                    position += fragmentScreenshot.Length - 2;
+                    fragmentScreenshot[fragmentScreenshot.Length - 2] = (byte)(count_array);
+
+                    fragments.Add(fragmentScreenshot);
+                }
+
+                for (int i = 0; i < count_array; i++)
+                {
+                    socket.SendTo(fragments[i], ipEndPoint);
+                    Thread.Sleep(5);
+                }
+
+            } while (true);
+        }
+
+        private static byte[] MakeScreenshot()
+        {
+            Bitmap bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            Graphics gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size,
+                CopyPixelOperation.SourceCopy);
+            return CompressBitmap(bmpScreenshot);
+        }
+
+        private static byte[] CompressBitmap(Bitmap bmp)
+        {
+            using (var zipped = new MemoryStream())
+            {
+                using (var gzip = new GZipStream(zipped, CompressionMode.Compress))
+                    bmp.Save(gzip, ImageFormat.Bmp);
+
+                return zipped.ToArray();
+            }
+        }
+    }
+}
---
+++ UDP_Server/UDP_Server/Form.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace UDP_Server
+{
+   
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+        }
+
+
+        private static int portReceive = 16000;
+
+        private Socket socket = new Socket(AddressFamily.InterNetwork,
+            SocketType.Dgram, ProtocolType.Udp);
+
+        private byte[] fragmentScreenshot = new byte[UInt16.MaxValue];
+
+        public List<byte[]> fragments = new List<byte[]>();
+
+        public int count_array = 0;
+
+        private static IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, portReceive);
+
+        private EndPoint endPoint = ipEndPoint;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            socket.Bind(endPoint);
+            socket.BeginReceiveFrom(fragmentScreenshot, 0, fragmentScreenshot.Length, SocketFlags.None,
+                ref endPoint, new AsyncCallback(ReceiveCallback), socket);
+        }
+
+
+        public void ReceiveCallback(IAsyncResult ar)
+        {
+                while (true)
+                {               
+                    int nFragment = 0;
+                    short value;
+                    do
+                    {
+                        byte[] recieve_bytes = new byte[65002];
+
+                        int num_bytes = socket.ReceiveFrom(recieve_bytes, ref endPoint);
+                   
+                        count_array += num_bytes - 2; 
+                        byte[] fragment_bytes = new byte[num_bytes - 2];
+                        byte[] value_bytes = new byte[2];
+
+                        Array.Copy(recieve_bytes, num_bytes - 2, value_bytes, 0, 2);
+                        Array.Copy(recieve_bytes, 0, fragment_bytes, 0, fragment_bytes.Length);
+
+                        value = BitConverter.ToInt16(value_bytes, value_bytes.Length - 2);
+                        fragments.Add(fragment_bytes);
+
+                        nFragment++;
+                    } while (nFragment != value);
+
+                    byte[] screenshot_bytes = new byte[count_array];
+
+                    for (int i = 0; i < fragments.Count; i++)
+                    {
+                        Array.Copy(fragments[i], 0, screenshot_bytes, i * 65000, fragments[i].Length);
+                    }
+
+                    byte[] result = Decompress(screenshot_bytes);
+
+                    MemoryStream memory_stream = new MemoryStream(result);
+                    System.Drawing.Bitmap bmp =
+                       (System.Drawing.Bitmap)System.Drawing.Image.FromStream(memory_stream);
+
+                    pictureBox1.Image = bmp;
+
+                    fragments.Clear();
+                    count_array = 0;
+                }
+        }
+
+
+        static byte[] Decompress(byte[] full_array)
+        {
+                using (var compressedMs = new MemoryStream(full_array))
+                {
+                    using (var decompressedMs = new MemoryStream())
+                    {
+                        using (var gzs = new BufferedStream(new GZipStream(compressedMs,
+                            CompressionMode.Decompress), 4096))
+                        {
+                            gzs.CopyTo(decompressedMs);
+                        }
+                        return decompressedMs.ToArray();
+                    }
+                }
+        }
+
+
+      
+
+        
+    }   
+}
